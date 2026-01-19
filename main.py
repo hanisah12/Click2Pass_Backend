9cb0335c76b7b82a6704e13e59839830a0de24e2
hanisah12 <siddhihanisah2007@gmail.com>
2026-01-19 17:36:56 +0530

    Changed the code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.user import router as user_router
 from routers.routers_pass import router as pass_router
 from routers.message import router as message_router
@@ -6,11 +7,25 @@ from db.database import Base, engine
 
 app = FastAPI(title="Bus Pass Booking API")
 
+
 Base.metadata.create_all(bind=engine)
+
+
+origins = ["http://127.0.0.1:5503", "http://localhost:5503"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
 app.include_router(pass_router)
 app.include_router(message_router)
+
 
 @app.get("/")
 def root():
